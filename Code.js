3966f26f0d316e6e2a05b7c931f7566ef1cd43f1
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-06-15 01:29:21 +0530

    Added high, low and open params

--- Code.js
+++ Code.js
@@ -2,7 +2,7 @@ function getStockPrices() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   
   // Clear previous data
-  sheet.getRange('B2:E').clearContent();
+  sheet.getRange('B2:G').clearContent();
   
   // Get stock symbols from column A
   var tickers = sheet.getRange('A2:A').getValues();
@@ -14,10 +14,16 @@ function getStockPrices() {
       var priceFormula = '=GOOGLEFINANCE("' + ticker + '","price")';
       var changeFormula = '=GOOGLEFINANCE("' + ticker + '","changepct")';
       var volumeFormula = '=GOOGLEFINANCE("' + ticker + '","volume")';
+      var highFormula = '=GOOGLEFINANCE("' + ticker + '","high")';
+      var lowFormula = '=GOOGLEFINANCE("' + ticker + '","low")';
+      var openFormula = '=GOOGLEFINANCE("' + ticker + '","open")';
       
       sheet.getRange(i + 2, 2).setFormula(priceFormula); // Column B for Price
       sheet.getRange(i + 2, 3).setFormula(changeFormula); // Column C for Change
       sheet.getRange(i + 2, 4).setFormula(volumeFormula); // Column D for Volume
+      sheet.getRange(i + 2, 5).setFormula(highFormula); // Column E for High
+      sheet.getRange(i + 2, 6).setFormula(lowFormula); // Column F for Low
+      sheet.getRange(i + 2, 7).setFormula(openFormula); // Column G for Open
     }
   }
   
@@ -28,10 +34,16 @@ function getStockPrices() {
   var prices = sheet.getRange('B2:B').getValues();
   var changes = sheet.getRange('C2:C').getValues();
   var volumes = sheet.getRange('D2:D').getValues();
+  var highs = sheet.getRange('E2:E').getValues();
+  var lows = sheet.getRange('F2:F').getValues();
+  var opens = sheet.getRange('G2:G').getValues();
   
   sheet.getRange('B2:B').setValues(prices);
   sheet.getRange('C2:C').setValues(changes);
   sheet.getRange('D2:D').setValues(volumes);
+  sheet.getRange('E2:E').setValues(highs);
+  sheet.getRange('F2:F').setValues(lows);
+  sheet.getRange('G2:G').setValues(opens);
 }
 
 function onOpen() {
